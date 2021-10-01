f0267be371693059a38572d9aeb740cad79ae793
yeyongping <burnedfrog@163.com>
2021-10-01 10:19:07 +0800

    Initial Commit

---
+++ HloggingDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HloggingDemo
+//
+//  Created by 叶永平 on 2021/10/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
